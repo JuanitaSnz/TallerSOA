690dc858c55153f0cb098ef5c844df6951d1c433
Juanita Sanabria <93340989+JuanitaSnz@users.noreply.github.com>
2023-11-13 12:48:18 -0500

    crossOrigins front

--- src/main/java/com/example/tallersoap/controller/CountWordController.java
+++ src/main/java/com/example/tallersoap/controller/CountWordController.java
@@ -4,13 +4,11 @@ import com.example.tallersoap.services.CountWords;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/wordCount")
 
--- src/main/java/com/example/tallersoap/controller/ReadController.java
+++ src/main/java/com/example/tallersoap/controller/ReadController.java
@@ -2,11 +2,9 @@ package com.example.tallersoap.controller;
 
 import com.example.tallersoap.services.Read;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/read")
 public class ReadController {
